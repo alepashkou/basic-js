1a7a2a03ae2aba5a679f0b7f4273e5ea67aed3ce
Alexander Pashkou <ale.pashkou@gmail.com>
2021-09-06 21:58:47 +0300

    init: comleate 2 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,9 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(activity) {
+  if (isNaN(activity) || typeof activity != 'string') {return false;}
+    activity = parseFloat(activity);
+  if (isNaN(activity) || activity > MODERN_ACTIVITY || activity <= 0) {return false;}
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD));
 }
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(array) {
+  let count = 0;
+  array.forEach(function(item, i){
+  item.forEach(function(text){
+  if (text === '^^'){count++}
+  })
+  })
+  return count;
 }

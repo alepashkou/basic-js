4edcd06c54f49978d5153fb539f3b209edfaeadf
Alexander Pashkou <ale.pashkou@gmail.com>
2021-10-03 20:08:34 +0300

    feat: add email task

--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -10,7 +10,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-export default function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getEmailDomain(email) {
+  if(email.includes('@.')){
+    return 'usual.com'
+  }
+  let adress = email.split('@');
+  return adress[1]
 }

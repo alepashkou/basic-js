abf8bd87f83bdf2248a1ddb30d5a0a4afef8ddf3
Alexander Pashkou <ale.pashkou@gmail.com>
2021-10-04 08:58:40 +0300

    feat: Vagner ciper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,79 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+ const alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+constructor (type = true) {
+    return this.type = type;
+}
+
+encrypt(string, key) {
+
+    if (!string || !key) {throw Error("Incorrect arguments!")}
+
+    string = string.toUpperCase().split("");    
+    key = key.toUpperCase();
+    var sum = 0, answer = "";
+  
+    string.forEach(el => {
+      const index = alpha.indexOf(el);
+
+      if (index == -1) {
+        answer += el;
+      } else {
+        let letter = key[sum % key.length];
+        let indexLetter = alpha.indexOf(letter);
+        let num;
+        if (index + indexLetter >= 26){
+          num = index + indexLetter - 26
+        }else {
+          num = index + indexLetter
+        }
+        sum += 1;
+        answer += alpha[num];
+      }
+    });
+    
+    if (!this.type) {
+      return answer.split("").reverse().join("")
+    } else {
+      return answer
+    }
+
+}
+
+  decrypt(string, key) {
+    if (!string || !key) {throw new Error("Incorrect arguments!")}
+
+    string = string.toUpperCase().split("");    
+    key = key.toUpperCase();
+    var sum = 0, answer = "";
+
+    string.forEach(el => {
+      const index = alpha.indexOf(el);
+
+      if (index == -1) {
+        answer += el;
+      } else {
+        let letter = key[sum % key.length];
+        let indexLetter = alpha.indexOf(letter);
+        let num;
+        if(index < indexLetter ){
+          num = 26 - indexLetter + index
+        }else{
+          num = index - indexLetter
+        }
+        sum += 1;
+        answer += alpha[num];
+      }
+    });
+
+    if (!this.type) {
+      return answer.split("").reverse().join("")
+    } else {
+      return answer
+    }
+    
   }
 }

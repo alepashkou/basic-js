5e63f4ac82fefc1196129949e7af37402e8e5710
Alexander Pashkou <ale.pashkou@gmail.com>
2021-10-04 13:25:43 +0300

    feat: add st delete num

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -9,9 +9,12 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * @example
  * For n = 152, the output should be 52
- *
+ * assert.strictEqual(deleteDigit(152), 52);
+    assert.strictEqual(deleteDigit(1001), 101);
+    assert.strictEqual(deleteDigit(10), 1);
+    assert.strictEqual(deleteDigit(222219), 22229);
+    assert.strictEqual(deleteDigit(109), 19);
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(num) {
+  return String(num).replace(Math.min(...(String(num).split(""))), '')
 }
